3a0c3ff5af1368ebbb7df8a3cdf7c96fafdeb765
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2016-10-27 14:11:01 +0200

    removed some deprecated files

--- models/instance.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var instanceSchema = mongoose.Schema({
-    mention_count: Number,
-    name: String,
-    url: String
-});
-
-module.exports = mongoose.model('Instance', instanceSchema);
